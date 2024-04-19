06f5024256c01e885605e067fbb6ac2fe4db18e1
Firsova Polina <firpoly@gmail.com>
2024-04-19 23:37:39 +0300

    внесены исправления

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,50 +15,9 @@ public class Main {
 
         try {
             final var server = new Server();
+            server.setVlidPaths(validPaths);
             server.listen(4532);
-            while (true) {
-                // read only request line for simplicity
-                // must be in form GET /path HTTP/1.1
-                server.startListen();
-                final var requestLine = server.read();
-                final var parts = requestLine.split(" ");
 
-                if (parts.length != 3) {
-                    continue;
-                }
-
-                final var path = parts[1];
-                if (!validPaths.contains(path)) {
-                    continue;
-                }
-
-                final var filePath = Path.of(".", "public", path);
-                final var mimeType = Files.probeContentType(filePath);
-                // special case for classic
-                if (path.equals("/classic.html")) {
-                    final var template = Files.readString(filePath);
-                    final var content = template.replace(
-                            "{time}",
-                            LocalDateTime.now().toString()
-                    ).getBytes();
-
-                    server.write("HTTP/1.1 200 OK\r\n" +
-                            "Content-Type: " + mimeType + "\r\n" +
-                            "Content-Length: " + content.length + "\r\n" +
-                            "Connection: close\r\n" +
-                            "\r\n",content);
-                    continue;
-                }
-
-                final var length = Files.size(filePath);
-                final var content =Files.readAllBytes(filePath);
-                server.write("HTTP/1.1 200 OK\r\n" +
-                        "Content-Type: " + mimeType + "\r\n" +
-                        "Content-Length: " + length + "\r\n" +
-                        "Connection: close\r\n" +
-                        "\r\n",content);
-
-            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -3,31 +3,108 @@ package org.example;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class Server {
     Socket socket;
     ServerSocket serverSocket;
     BufferedReader in;
     BufferedOutputStream out;
+    List<String> setVlidPaths;
+
     Server() throws IOException {
         serverSocket = new ServerSocket();
     }
+
+    public void setVlidPaths(List<String> setVlidPaths) {
+        this.setVlidPaths = setVlidPaths;
+    }
+
     public void listen(int port) throws IOException {
         this.serverSocket = new ServerSocket(port);
+        listenPort();
     }
-    public void startListen() throws IOException {
-        this.socket = this.serverSocket.accept();
-        in =new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+
+    public void startListen(ServerSocket serverSocket) throws IOException {
+        this.socket = serverSocket.accept();
+        in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
         out = new BufferedOutputStream(this.socket.getOutputStream());
+
     }
-    public void write (String text, byte [] content) throws IOException {
+
+    public void listenPort() throws IOException {
+        int threadBound = 64;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(64);
+        for (int i = 0; i < threadBound + 1; i++) {
+            startListen(this.serverSocket);
+            Future future = executorService.submit(new Callable() {
+                public Object call() throws Exception {
+                    dataHandling(read());
+                    return "result";
+                }
+            });
+
+        }
+        executorService.shutdown();
+
+//        }
+    }
+
+    public void dataHandling(String text) throws IOException {
+        String requestLine = text;
+        final var parts = requestLine.split(" ");
+
+        if (parts.length == 3) {
+            final var path = parts[1];
+            if (!!this.setVlidPaths.contains(path)) {
+                final var filePath = Path.of(".", "public", path);
+                final var mimeType = Files.probeContentType(filePath);
+                // special case for classic
+                if (path.equals("/classic.html")) {
+                    final var template = Files.readString(filePath);
+                    final var content = template.replace(
+                            "{time}",
+                            LocalDateTime.now().toString()
+                    ).getBytes();
+
+                    this.write("HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + content.length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n", content);
+
+                } else {
+                    final var length = Files.size(filePath);
+                    final var content = Files.readAllBytes(filePath);
+                    this.write("HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n", content);
+                }
+
+
+            }
+        }
+
+
+    }
+
+    public void write(String text, byte[] content) throws IOException {
         out.write(text.getBytes());
         if (content.length != 0) {
             out.write(content);
         }
         out.flush();
     }
-    public String read () throws IOException {
+
+    public String read() throws IOException {
         return in.readLine();
     }
 
